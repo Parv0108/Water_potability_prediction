0f99f05df72b13860c87afa254b340ce4e6378c6
Parv0108 <parv.vachhani01082006@gmail.com>
2026-06-22 08:48:13 +0530

    Delete run_pipeline.cmd

--- run_pipeline.cmd
+++
@@ -1,4 +0,0 @@
-python scr/feature_engineering.py --path=params.yaml
-python scr/train_test_split.py --path=params.yaml
-python scr/training.py --path=params.yaml
-python scr/evalution.py --path=params.yaml
